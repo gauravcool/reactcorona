589af692bc27d5aa472c32ce4269015f618416fc
gaurav kumar <gaurav.ac.kumar@oracle.com>
2020-05-02 17:05:07 +0530

    Updates

---
+++ precache-manifest.f6d7a0169486483b90cfe28b02971a0d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bb29932c828cb3b7c87d79c7c264b17",
+    "url": "/reactcorona/index.html"
+  },
+  {
+    "revision": "f2d11cc76da732cff69b",
+    "url": "/reactcorona/static/css/main.da09a333.chunk.css"
+  },
+  {
+    "revision": "2510a9e0989cc7440a2c",
+    "url": "/reactcorona/static/js/2.4880063e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/reactcorona/static/js/2.4880063e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2d11cc76da732cff69b",
+    "url": "/reactcorona/static/js/main.96e9471b.chunk.js"
+  },
+  {
+    "revision": "13edc0ec9e06d9f59610",
+    "url": "/reactcorona/static/js/runtime-main.0642eac0.js"
+  },
+  {
+    "revision": "bc71574bb84f2ea467520ca59384b532",
+    "url": "/reactcorona/static/media/SARS-CoV-2_without_background.bc71574b.png"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/reactcorona/static/media/image.d7265326.png"
+  }
+]);
